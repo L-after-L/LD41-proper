a00453c45175890c6ec70dc7e93b9019e97bcac0
grumpypenny <ajitesh.misra.14@gmail.com>
2018-04-21 19:54:08 -0400

    working on Inventory

---
+++ Ludum Dare 41/Assets/Scripts/Inventory.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+	public List<Stats> things = new List<Stats>();
+
+	public void Add(Stats item) {
+		things.Add(item);
+	}
+
+	public void Remove(Stats item) {
+		things.Remove(item);
+	}
+}
---
+++ Ludum Dare 41/Assets/Scripts/Stats.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Pickups/Items")]
+public class Stats : ScriptableObject {
+
+	new public string name = "new item";
+
+	public int value = 0;
+
+	public string description = "It's probably useful";
+}
