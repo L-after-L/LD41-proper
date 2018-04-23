32b419725304d38c1f5323d8bf8ba82c0ae1df4b
awesomegameguy <maxlou111@gmail.com>
2018-04-22 23:08:10 -0400

    HealthBar stuff

---
+++ Ludum Dare 41/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+    public Health hp;
+    public Image healthBar; 
+
+    private float health;
+	// Use this for initialization
+	void Start () {
+        hp = FindObjectOfType<PlayerMovement>().gameObject.GetComponent<Health>();
+        healthBar = GetComponent<Image>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void LateUpdate()
+    {
+        health = hp.readOnlyHealthPoints;
+        healthBar.fillAmount = health / 100;
+    }
+}
