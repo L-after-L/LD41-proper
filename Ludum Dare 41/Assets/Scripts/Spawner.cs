d99716f416ff3a29723adddef9fb8b0cf4b63780
grumpypenny <ajitesh.misra.14@gmail.com>
2018-04-22 20:05:52 -0400

    Added Spawn Object

---
+++ Ludum Dare 41/Assets/Scripts/Spawner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public AIMovement[] enemies;
+
+	// Use this for initialization
+	void Start () {
+		int rand = Random.Range(0, enemies.Length - 1);
+
+		Instantiate(enemies[rand],transform.position, transform.rotation);	
+	}
+}
